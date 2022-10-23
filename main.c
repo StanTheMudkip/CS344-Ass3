e7e71e4cb6fa12d4d61aa04c9307e5af88bcce5e
Stanley Hale <halesta@os1.engr.oregonstate.edu>
2022-10-23 16:31:18 -0700

    Prepped input string for tokenizing

--- main.c
+++ main.c
@@ -30,11 +30,22 @@ struct cmmd {
 
 void prompt() {
 	char str_input[420];
+	memset(str_input, '\0', sizeof(str_input));			//Clears the string memory to only NULL
 	printf(": ");										//Character to denote that we are ready for a new command.
 	
 	//Read in a line from the user
-	fgets(str_input, 420, stdin);
-	printf("The command you input is: %s\n", str_input);
+	fgets(str_input, 419, stdin);						//take 419 just to make sure we have enough room in the string for a NULL terminator
+	//printf("The command you input is: %s\n", str_input);
+	//fflush(stdout);
+	
+	//check if we have filled the string and that it is greater than 1
+	if( (str_input[strlen(str_input)-1] == '\n') && (strlen(str_input) > 1) )
+	{
+		str_input[strlen(str_input)-1] = '\0';			//Replace the newline with a null terminator so that we can tokenize this string and parse it.
+	}
+	//printf("The command you input w/out a (natural) '\\n' is: %s\n", str_input);
+	/fflush(stdout);
+	
 	
 }
 
