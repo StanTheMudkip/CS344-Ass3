a52fe0a8be832c57042dfe149635936cfd6264af
Stanley Hale <halesta@os1.engr.oregonstate.edu>
2022-10-23 21:12:43 -0700

    Got it so that it constantly repompts for input after each getline!!! Now just need to add functionality for commands I think.

--- main.c
+++ main.c
@@ -24,10 +24,32 @@
 typedef struct cmmd command;
 //Structure of commands that will be used
 struct cmmd {
-		int argc;															//Keeps count of the number of arguments
-		char* argv[420];													//Set max of arguments to 420 characters (includes the actual command along side arguments)
-		int BackGround;														//Keeps track if this is a background proccess?
+		int argc;																//Keeps count of the number of arguments
+		char* argv[420];														//Set max of arguments to 420 characters (includes the actual command along side arguments)
+		int BackGround;															//Keeps track if this is a background proccess?
 };
+
+//Destructor
+void freeCommand(command* cmd)
+{
+	//Go through all the arguments starting from the last argument.
+	while(cmd->argc != 0)														
+	{
+		//printf("In loop\n");
+		//fflush(stdout);
+		
+		if (!cmd->argv[cmd->argc])												//If the argument exists, free it
+		{
+			//printf("Freeing\n");
+			free(cmd->argv[cmd->argc]);
+			//Decrement the argument counter
+			cmd->argc -= 1;
+		}
+	}
+	
+	//Reset the allocated memory to NULL
+	memset(cmd->argv, '\0', sizeof(cmd->argv));
+}
 
 
 
@@ -48,25 +70,27 @@ void printCommand(command cmd) {
 	printf("\n");
 }
 
-
 void prompt(command* cmd) {
+	//NEED TO RESET THE COMMAND CMD SO THAT IS WONT SAVE WHAT IT WAS!!!!!!
+	freeCommand(cmd);
+	
 	char* token;
-	char* saveptr;															//Using this variable for strtok_r
+	char* saveptr;																//Using this variable for strtok_r
 	
 	char str_input[420];
-	memset(str_input, '\0', sizeof(str_input));								//Clears the string memory to only NULL
-	printf(": ");															//Character to denote that we are ready for a new command.
+	memset(str_input, '\0', sizeof(str_input));									//Clears the string memory to only NULL
+	printf(": ");																//Character to denote that we are ready for a new command.
 	fflush(stdout);
 	
 	//Read in a line from the user
-	fgets(str_input, 419, stdin);											//take 419 just to make sure we have enough room in the string for a NULL terminator
+	fgets(str_input, 419, stdin);												//take 419 just to make sure we have enough room in the string for a NULL terminator
 	//printf("The command you input is: %s\n", str_input);
 	//fflush(stdout);
 	
 	//check if we have filled the string and that it is greater than 1
 	if( (str_input[strlen(str_input)-1] == '\n') && (strlen(str_input) > 1) )
 	{
-		str_input[strlen(str_input)-1] = '\0';								//Replace the newline with a null terminator so that we can tokenize this string and parse it.
+		str_input[strlen(str_input)-1] = '\0';									//Replace the newline with a null terminator so that we can tokenize this string and parse it.
 	}
 	//printf("The command you input w/out a (natural) '\\n' is: %s\n", str_input);
 	//fflush(stdout);
@@ -74,8 +98,9 @@ void prompt(command* cmd) {
 	
 	//Parse the input
 	
-	token = strtok_r(str_input, " ", &saveptr); 							//Grabs the first argument (the actual command) 
+	token = strtok_r(str_input, " ", &saveptr); 								//Grabs the first argument (the actual command) 
 	//printStr(token);
+	
 	
 	while(token != NULL) 
 	{
@@ -83,9 +108,9 @@ void prompt(command* cmd) {
 		cmd->argv[cmd->argc] = malloc( sizeof(strlen(token)) ); 				//allocate memory for the new command
 		memset( cmd->argv[cmd->argc], '\0', sizeof(cmd->argv[cmd->argc]) );		//Clears the allocated string memory for copying.
 		strcpy(cmd->argv[cmd->argc], token);									
-		cmd->argc += 1;														//increment the number of arguments
+		cmd->argc += 1;															//increment the number of arguments
 		
-		token = token = strtok_r(NULL, " ", &saveptr); 						//Grab the next argument (if it doesnt exist it will be NULL)
+		token = token = strtok_r(NULL, " ", &saveptr); 							//Grab the next argument (if it doesnt exist it will be NULL)
 		
 		/*Prints each token that is aquired.
 		if(token != NULL) 
@@ -104,40 +129,44 @@ void prompt(command* cmd) {
 
 
 int main() {
-	command cmd;															//Create an instance of my command struct
-	cmd.argc = 0;															//Set the unitialized argument count to 0;
+	command cmd;																//Create an instance of my command struct
+	cmd.argc = 0;																//Set the unitialized argument count to 0;
+	int again = 0;
 	
-	
-	//Prompt user for input
-	prompt(&cmd);
-	//printCommand(cmd);
-	
-	//check if the command (first argument) is a newline
-	if( strcmp(cmd.argv[0], "\n") )
+	while (again == 0) 															//Repeat prompting and handling commands
 	{
-		//check if the command (first charcter of the first argument) is a comment!
-		if(cmd.argv[0][0] != '#')
+		//Prompt user for input
+		prompt(&cmd);
+		//printCommand(cmd);
+		
+		//check if the command (first argument) is a newline and (first charcter of the first argument) is a comment!
+		if( strcmp(cmd.argv[0], "\n") && (cmd.argv[0][0] != '#') )
 		{
 			//This is where we can check for our versions of cd, exit, and status
-			if( strcmp(cmd.argv[0], "exit") == 0)								//If we enter exit, we just exit the shell (aka this program/process)
+			if( !strcmp(cmd.argv[0], "exit") )									//If we enter exit, we just exit the shell (aka this program/process)
 			{
+				printf("EXITING smallsh\n");
+				fflush(stdout);
+				again = 1;
 				return 0;
 			}
-			else if( strcmp(cmd.argv[0], "cd") == 0 )
+			else if( !strcmp(cmd.argv[0], "cd") )
 			{
 				printf("Changing Directory\n");
 				fflush(stdout);
 			}
-			else if( strcmp(cmd.argv[0], "status") == 0 )
+			else if( !strcmp(cmd.argv[0], "status") )
 			{
 				printf("Showing status\n");
 				fflush(stdout);
 			}
-			//Then handle other commands?
-			
+			else 
+			{
+				//Then handle other commands
+			}
+		
 		}
 	}
-	
 	
 	return 0;
 }
